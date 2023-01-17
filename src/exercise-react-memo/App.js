5171427bada28a7d96d76007e7dbe04b795715fe
NguyenMinhTuan <tuannb0mmm.2x1y9z@gmail.com>
2023-01-17 16:58:00 +0700

    update

--- src/exercise-react-memo/App.js
+++ src/exercise-react-memo/App.js
@@ -7,11 +7,66 @@
  */
 
 import React from "react";
+import Content from "./Content";
 
-export default function ExerciseReactMemoHOC() {
+const styleDiv = {
+  margin: '0 2%',
+  marginBottom: '16px',
+  border: '1px solid #c4c4c4',
+  paddingBottom: '16px',
+  borderRadius: '4px',
+  width: '96%',
+  minHeight:'100px',
+  boxSizing: 'border-box',
+  display: 'flex',
+  flexDirection: 'column',
+  alignContent: 'center',
+  alignItems: 'center',
+  flexWrap: 'nowrap',
+}
+
+const styleButton = {
+  fontSize: '16px',
+  color: '#fb7800',
+  fontWeight:"600",
+  height: '40px',
+  width: '50%',
+  margin: '0 2%',
+  marginTop: '16px',
+  backgroundColor:"#fff",
+  borderRadius:"4px",
+  cursor:"pointer",
+  display:'flex',
+  alignItems: 'center',
+  placeContent: 'center',
+  padding:"8px",
+  border:"1px solid #fb7800"
+}
+
+export default function ExerciseReactMemoHOC (){
+  const [mounted, setMounted] = React.useState(false)
   return (
-    <div>
-      abc
-    </div>
+    <React.Fragment>
+      <div style={styleDiv}>
+        <h2> React Memo </h2>
+        <button
+        style={styleButton}
+          onClick={()=>setMounted(!mounted)}
+        >
+          Show Content Exercise
+        </button>
+          <div
+          style={
+            {
+              display: 'flex',
+            }
+          }
+        >
+        </div>
+        {
+          mounted && <Content/>
+        }
+      </div>      
+    </React.Fragment>
   )
 }
---
+++ src/exercise-react-memo/Content.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default React.memo(function Content ({number}) {
+    return (
+        <h2>
+            Đây là nội dung bài học!
+        </h2>
+    )
+})
